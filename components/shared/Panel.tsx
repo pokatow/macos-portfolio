bcb06cfec255a7cdb371d1395de8b0d46b3290cd
pokatow <0xpokatow@gmail.com>
2022-09-08 09:47:52 +0700

    Fix double tap

--- components/shared/Panel.tsx
+++ components/shared/Panel.tsx
@@ -35,36 +35,38 @@ const Panel: React.FunctionComponent<{
       {!isMinimize && (
         <DraggableCore handle="span" onStart={onStartHandler}>
           <div
-            className={`absolute top-1/4 left-1/3 z-40 mx-auto flex flex-col overflow-hidden rounded-xl shadow-xl ${extraClass}`}
+            className={` absolute top-1/3 right-1/3 z-40 flex flex-col overflow-hidden rounded-xl shadow-xl ${extraClass}`}
           >
-            <span className="">
-              <div className="relative flex items-center justify-center px-3 py-2 border-b-2 bg-white/80 backdrop-blur-3xl">
-                <div className="absolute inset-y-0 left-0 flex items-center px-3 space-x-2">
-                  <div
-                    className="w-3 h-3 bg-red-400 rounded-full cursor-pointer"
-                    onClick={close}
-                    onTouchStart={close}
-                    title="Exit"
-                  ></div>
-                  <div
-                    className="w-3 h-3 bg-yellow-400 rounded-full cursor-pointer "
-                    onClick={minimize}
-                    onTouchStart={minimize}
-                    title="Minimize"
-                  ></div>
-                  <div
-                    className="w-3 h-3 bg-green-400 rounded-full cursor-pointer"
-                    onClick={minimize}
-                    onTouchStart={minimize}
-                    title="Minimize too xD"
-                  ></div>
-                </div>
-                <div className="flex items-center justify-center flex-1">
-                  {title}
-                </div>
+            <div className="relative">
+              <div className="absolute top-0 left-0 z-30 flex items-center pt-4 pl-4 space-x-2">
+                <div
+                  className="w-3 h-3 bg-red-400 rounded-full cursor-pointer"
+                  onClick={close}
+                  onTouchStart={close}
+                  title="Exit"
+                ></div>
+                <div
+                  className="w-3 h-3 bg-yellow-400 rounded-full cursor-pointer "
+                  onClick={minimize}
+                  onTouchStart={minimize}
+                  title="Minimize"
+                ></div>
+                <div
+                  className="w-3 h-3 bg-green-400 rounded-full cursor-pointer"
+                  onClick={minimize}
+                  onTouchStart={minimize}
+                  title="Minimize too xD"
+                ></div>
               </div>
-            </span>
-            <div className="flex flex-col flex-1 bg-white">{children}</div>
+              <span className="">
+                <div className="relative flex items-center justify-center px-3 py-2 border-b-2 bg-white/80 backdrop-blur-3xl">
+                  <div className="flex items-center justify-center flex-1">
+                    {title}
+                  </div>
+                </div>
+              </span>
+              <div className="flex flex-col flex-1 bg-white">{children}</div>
+            </div>
           </div>
         </DraggableCore>
       )}
